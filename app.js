47f1351fc28d046163427c209ffdb6b8585b3ddd
David Johnson <davidjohnson1297@gmail.com>
2020-01-15 18:49:01 -0600

    added some parsing

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const multipart = require('connect-multiparty')
+const multipartMiddleware = multipart()
+
+const bodyParser = require('body-parser')
+
 const data = require('./data.js')
 
 app.use(require('morgan')('dev'))
-
+app.use(bodyParser.json())
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.status(200).send('OK').end()
 })
+app.post('/login', urlencodedParser, (req, res) => {
+    console.log(req.body)
+    // if (req.body === 'user') {
+    res.status(200).json(data.userData).end()
+    // } else {
+    //     res.status(400).json({
+    //         message: 'no way jose'
+    //     })
+    // }
+})
 app.get('/login', (req, res) => {
     res.status(200).json(data.userData).end()
 })
 app.get('/clinicians', (req, res) => {
-    console.log(data.data)
-    res.status(200).json({clinicians: data.data}).end()
+    res.status(200).json({clinicians: data.clinicianData}).end()
+})
+app.get('/patients', (req, res) => {
+    res.status(200).json({ patients: data.patientData }).end()
 })
 
 
